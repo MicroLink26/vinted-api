e5a76578293e5e536fa54a66867a28ebb969ac9d
Mike <mic.roche@gmail.com>
2023-05-24 17:58:13 +0200

    process.env.CLOUDINARY_CLOUD_NAME updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI);
 
 // Connexion à l'espace de stockage cloudinary
 cloudinary.config({
-  cloud_name: "lereacteur",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
