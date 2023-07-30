f778e8691373948fad262f9e39d29abdb16dd180
Mike <mic.roche@gmail.com>
2023-07-30 10:01:29 +0200

    fix payment

--- routes/offer.js
+++ routes/offer.js
@@ -18,9 +18,6 @@ const Offer = require("../models/Offer");
 // Import du middleware isAuthenticated
 const isAuthenticated = require("../middleware/isAuthenticated");
 
-// Import des datas (ne pas en tenir compte, cela sert au reset de la BDD entre 2 sessions de formation)
-const products = require("../data/products.json");
-
 // Route qui nous permet de récupérer une liste d'annonces, en fonction de filtres
 // Si aucun filtre n'est envoyé, cette route renverra l'ensemble des annonces
 router.get("/offers", async (req, res) => {
