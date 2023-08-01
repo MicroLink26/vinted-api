384e7d4c91ee64bd27d80a0ef1b5d897abf533f5
Mike <mic.roche@gmail.com>
2023-08-01 21:26:31 +0200

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const morgan = require("morgan");
+
 const cloudinary = require("cloudinary").v2;
 
 const app = express();
